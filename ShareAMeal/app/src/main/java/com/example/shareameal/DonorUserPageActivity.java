d8635a702bd72261ba9990e6810cf42330414b23
Yeo Jun Wei <>
2021-06-17 09:09:10 +0800

    Fixed bugs

--- ShareAMeal/app/src/main/java/com/example/shareameal/DonorUserPageActivity.java
+++ ShareAMeal/app/src/main/java/com/example/shareameal/DonorUserPageActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
