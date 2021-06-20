9b0d439ed18cf5d77c42095febdf5da30a7de3ce
Yeo Jun Wei <>
2021-06-20 23:55:05 +0800

    1) Fixed address edit field in edit profile, 2) Added field to display distance away from donor in viewing interface

--- ShareAMeal/app/src/main/java/com/example/shareameal/RVDonorsAdapter.java
+++ ShareAMeal/app/src/main/java/com/example/shareameal/RVDonorsAdapter.java
@@ -1,10 +1,15 @@
 package com.example.shareameal;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,11 +18,25 @@ import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
 
@@ -36,9 +55,9 @@ public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         listFull = new ArrayList<>(list);
     }
 
-    public class DonorVH extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class DonorVH extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TextView txt_donor, txt_donor_address;
+        public TextView txt_donor, txt_donor_address, txt_donor_distance;
         public ImageView image;
         public OnDonorClickListener onDonorClickListener;
 
@@ -46,6 +65,7 @@ public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             super(itemView);
             txt_donor = itemView.findViewById(R.id.txt_donor);
             txt_donor_address = itemView.findViewById(R.id.txt_donor_address);
+            txt_donor_distance = itemView.findViewById(R.id.txt_donor_distance);
             image = itemView.findViewById(R.id.img_donor);
             this.onDonorClickListener = onDonorClickListener;
 
@@ -71,12 +91,41 @@ public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         DonorVH vh = (DonorVH) holder;
         // probably change to donor later
         User donor = list.get(position);
-        if(TextUtils.isEmpty(donor.getRestaurant())) {
+        if (TextUtils.isEmpty(donor.getRestaurant())) {
             vh.txt_donor.setText(donor.getName());
         } else {
             vh.txt_donor.setText(donor.getRestaurant());
         }
         vh.txt_donor_address.setText(donor.getAddress());
+
+        FirebaseUser recipient = FirebaseAuth.getInstance().getCurrentUser();
+        String recipientId = recipient.getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(recipientId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("Firebase", "Error getting data", task.getException());
+                } else {
+                    User user = task.getResult().getValue(User.class);
+                    double recipientAddressLat = user.getAddressLatitude();
+                    double recipientAddressLong = user.getAddressLongitude();
+                    double donorAddressLat = donor.getAddressLatitude();
+                    double donorAddressLong = donor.getAddressLongitude();
+                    float[] result = new float[1];
+
+                    // distanceBtwn is in metres
+                    Location.distanceBetween(recipientAddressLat, recipientAddressLong, donorAddressLat, donorAddressLong, result);
+                    float distanceBtwnInMetres = result[0];
+                    float distanceBtwnInKm = distanceBtwnInMetres / 1000;
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    df.setRoundingMode(RoundingMode.CEILING);
+                    String distanceStr = String.valueOf(df.format(distanceBtwnInKm)) + " km away";
+                    vh.txt_donor_distance.setText(distanceStr);
+                }
+            }
+        });
+
         if (donor.getImageUrl() == null) {
             vh.image.setImageResource(R.drawable.profile128px);
         } else {
@@ -112,7 +161,7 @@ public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             } else {
                 String filteredPattern = constraint.toString().toLowerCase().trim();
 
-                for (User user: listFull) {
+                for (User user : listFull) {
                     if (user.getRestaurant() == null || TextUtils.isEmpty(user.getRestaurant())) {
                         if (user.getName().toLowerCase().contains(filteredPattern)) {
                             filteredList.add(user);
@@ -139,4 +188,5 @@ public class RVDonorsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             notifyDataSetChanged();
         }
     };
+
 }
